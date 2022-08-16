edaba29f95d00456d1f0a3e2b3e6d12d339b41bc
Jorge López <80071604+jorgeloopzz@users.noreply.github.com>
2022-08-16 22:38:11 +0000

    Added more privacy settings

--- user.js
+++ user.js
@@ -20,6 +20,10 @@ user_pref("network.prefetch-next", false);
 user_pref("webgl.disabled", true);
 user_pref("dom.event.clipboardevents.enabled", false);
 user_pref("privacy.resistFingerprinting.letterboxing", true);
+user_pref("network.trr.mode", 2);
+user_pref("network.security.esni.enabled", true);
+user_pref("network.dns.echconfig.enabled", true);
+user_pref("network.dns.use_https_rr_as_altsvc", true);
 
 // Disable PiP
 user_pref("media.videocontrols.picture-in-picture.video-toggle.enabled", false);
