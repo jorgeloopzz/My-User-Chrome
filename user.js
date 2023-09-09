e08b42f3164d4c9f7a7b0af667ce68f2cb1369cd
jorgeloopzz <jorgeloopzz@protonmail.com>
2023-09-09 12:46:53 +0100

    Add user.js settings commented

--- user.js
+++ user.js
@@ -7,3 +7,61 @@ user_pref("layout.css.backdrop-filter.enabled", true);
 user_pref("widget.non-native-theme.scrollbar.style", 5);
 user_pref("layers.acceleration.force-enabled", true);
 user_pref("gfx.webrender.all", true);
+user_pref("browser.tabs.tabmanager.enabled", false);
+user_pref("extensions.unifiedExtensions.enabled", true);
+
+/*
+ * Additional settings I use in my user.js. Not necessary for customization.
+ * Don't uncomment if you don't know what they do. Also I don't recommend it.
+
+// Privacy settings
+user_pref("media.peerconnection.enabled", false);
+user_pref("privacy.resistFingerprinting", true);
+user_pref("privacy.trackingprotection.enabled", true);
+user_pref("media.navigator.enabled", false);
+user_pref("network.cookie.cookieBehavior", 1);
+user_pref("network.cookie.lifetimePolicy", 2);
+user_pref("network.dns.disablePrefetch", true);
+user_pref("network.prefetch-next", false);
+user_pref("webgl.disabled", true);
+user_pref("dom.event.clipboardevents.enabled", false);
+user_pref("privacy.resistFingerprinting.letterboxing", false);
+user_pref("network.trr.mode", 2);
+user_pref("network.security.esni.enabled", true);
+user_pref("network.dns.echconfig.enabled", true);
+user_pref("network.dns.use_https_rr_as_altsvc", true);
+user_pref("extensions.pocket.enabled", false);
+user_pref("privacy.firstparty.isolate", true);
+user_pref("geo.enabled", false);
+user_pref("media.eme.enabled", false);
+user_pref("browser.ping-centre.telemetry", false);
+user_pref("toolkit.telemetry.archive.enabled", false);
+user_pref("toolkit.telemetry.bhrPing.enabled", false);
+user_pref("toolkit.telemetry.enabled", false);
+user_pref("toolkit.telemetry.server", false);
+user_pref("toolkit.telemetry.prompted", 2);
+user_pref("toolkit.telemetry.rejected", true);
+user_pref("toolkit.telemetry.firstShutdownPing.enabled", false);
+user_pref("toolkit.telemetry.hybridContent.enabled", false);
+user_pref("toolkit.telemetry.newProfilePing.enabled", false);
+user_pref("toolkit.telemetry.reportingpolicy.firstRun", false);
+user_pref("toolkit.telemetry.shutdownPingSender.enabled", false);
+user_pref("toolkit.telemetry.unified", false);
+user_pref("toolkit.telemetry.unifiedIsOptIn", false);
+user_pref("toolkit.telemetry.updatePing.enabled", false);
+user_pref("extensions.experiments.enabled", false);
+user_pref("datareporting.healthreport.uploadEnabled", false);
+user_pref("browser.newtabpage.activity-stream.feeds.telemetry", false);
+user_pref("browser.newtabpage.activity-stream.telemetry", false);
+
+// Disable PiP
+user_pref("media.videocontrols.picture-in-picture.video-toggle.enabled", false);
+
+// Disable autoplay
+user_pref("media.autoplay.default", 5);
+user_pref("media.autoplay.blocking_policy", 2);
+user_pref("media.autoplay.allow-extension-background-pages", false);
+user_pref("media.autoplay.block-event.enabled", true);
+
+*/
+
